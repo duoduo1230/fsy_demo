d4e2a3c6c5f9babf7ac554df1293087dcb87254f
duoduo1230 <925015687@qq.com>
2024-09-06 14:11:06 +0800

    大禹下载文件备份

--- app_DAYU/_nuke/batch_download_file_tool/ors_download/download_RS_dailies_dialog.py
+++ app_DAYU/_nuke/batch_download_file_tool/ors_download/download_RS_dailies_dialog.py
@@ -8,7 +8,7 @@ import csv
 import time
 from datetime import datetime, timedelta
 import db.util
-from app import DAYU
+from app_DAYU import DAYU
 import datetime as dt
 import wrap_shotgun as ws
 from ui_center.qt import *
@@ -19,7 +19,7 @@ from config.const import OUTPUT_FORMAT_DICT
 from ui_center.widgets.MFolderWidget import MFolderWidget
 from ui_center.widgets.MItemViewFilterSetWidget import MItemViewFilterSetWidget
 from ui_center.widgets.message_box import MErrorMessageBox, MSuccessMessageBox
-from app._public.context_menu.folder_file.download_dailies.attr_config import COLOR_SPACE_CONFIG, META_CODEC_DICT
+from app_DAYU._public.context_menu.folder_file.download_dailies.attr_config import COLOR_SPACE_CONFIG, META_CODEC_DICT
 
 
 class DownloadDailiesDialog(QDialog):
