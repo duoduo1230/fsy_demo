58ffe229955d9447767872287ccd5e542ae87365
duoduo1230 <925015687@qq.com>
2024-09-18 11:04:52 +0800

    Revert "自习以及大禹的nuke studio工具"
    This reverts commit 04e08d3ec7713fc4abb6b7a588d1bc4a8b6ae2a5.

--- _app/studio/dayu_tool/solver/dayu_di_name_solver.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
-
-__author__ = 'andyguo'
-
-
-def di_filename_solver(task):
-    task._preset.properties()['dayu_di_filename'] = 'di_filename'
-    return task._preset.properties()['dayu_di_filename']
--- _app/studio/dayu_tool/spreadsheet_column/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : 1/23/2019 11:23 AM
-
-__author__ = 'pengyuxuan'
-
-import hiero
-import color_space_column
-hiero.ui.customColumn = color_space_column.CustomSpreadsheetColumns()
